1c16e0dabe31077f9e76eb1f3a77cfb862aa12c6
Jaber <Jaber@DESKTOP-NN2JLKO>
2018-12-16 15:36:50 +0330

    add Repository and UnitOfWork Pattern

---
+++ WeeklyNews/DAL/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeeklyNews.Models;
+
+namespace WeeklyNews.DAL
+{
+    public interface IUnitOfWork
+    {
+        IRepository<News> NewsRepository { get; }
+        IRepository<Category> CategoryRepository { get; }
+        void Commit();
+    }
+}
---
+++ WeeklyNews/DAL/UnitOfWork.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Objects;
+using System.Linq;
+using System.Web;
+using WeeklyNews.Models;
+
+namespace WeeklyNews.DAL
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ObjectContext _context;
+        private CategoryRepository _categoryRepository;
+        private NewsRepository _newsRepository;
+        public UnitOfWork(ObjectContext context)
+        {
+            if (context  == null)
+            {
+                throw new ArgumentNullException();
+            }
+            _context = context;
+        }
+        public IRepository<News> NewsRepository
+        {
+            get
+            {
+                if (_newsRepository == null)
+                {
+                    _newsRepository = new NewsRepository(_context);
+                }
+                return _newsRepository;
+            }            
+        }
+        public IRepository<Category> CategoryRepository
+        {
+            get
+            {
+                if (_categoryRepository == null)
+                {
+                    _categoryRepository = new CategoryRepository(_context);
+                }
+                return _categoryRepository;
+            }
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();            
+        }
+    }
+}
