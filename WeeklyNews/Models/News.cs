5f82fde1f2db8e1631d876008af04ff96ca6db1e
Jaber <Jaber@DESKTOP-NN2JLKO>
2018-12-16 13:12:01 +0330

    add Models and DbContext

---
+++ WeeklyNews/Models/News.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WeeklyNews.Models
+{
+    public class News
+    {
+        [Key]
+        public long Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public byte[] Image { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+    }
+}
